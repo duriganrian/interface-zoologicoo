a877b1864847ac17a3a824d374c8b018f4b7de52
Rian Durigan <durigansrian@gmail.com>
2024-05-10 11:07:44 -0300

    Criando a tabela

--- src/pages/Depoimentos/Depoimentos.jsx
+++ src/pages/Depoimentos/Depoimentos.jsx
@@ -1,16 +1,54 @@
-import './Depoimentos.css'
+import React, { useEffect, useState } from 'react';
 import Navegacao from '../../components/Navegacao/Navegacao';
+import Tabela from '../../components/Table/Table';
+import './Depoimentos.css';
 
 function Depoimentos() {
+    const [animais, setAnimais] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost:3000/listar-aves');
+                if (!response.ok) {
+                    throw new Error('Erro ao buscar servidor');
+                }
+                const listaAnimais = await response.json();
+                setAnimais(listaAnimais);
+            } catch (error) {
+                console.error('Erro: ', error);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     return (
         <>
             <Navegacao />
-            
-            <p style={{color: "#000000"}}>NADA AQUI AINDA</p>
+            <div className='cnt-tb'>
+                {animais.length > 0 ? (
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th scope="col" >Nome</th>
+                                <th scope="col" >Gênero</th>
+                                <th scope="col" >Espécie</th>
+                                <th scope="col" >Envergadura</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {animais.map(animal => (
+                                <Tabela key={animal.idanimal} animal={animal} />
+                            ))}
+                        </tbody>
+                    </table>
+                ) : (
+                    <p>Carregando...</p>
+                )}
+            </div>
         </>
-
     );
 }
 
-export default Depoimentos
+export default Depoimentos;
